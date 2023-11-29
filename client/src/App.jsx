e0f8d8ce104498f07e0d2a101a10d2d2b6af158d
[ArhanAli1] <[aliarhan646@gmail.com]>
2023-11-29 15:53:05 +0530

    update the header and make profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './pages/Home';
-import SignUp from './pages/SignUp.jsx';
-import SignIn from './pages/SignIn.jsx';
+
+import SignUp from './pages/SignUp.jsx'
 import Profile from './pages/Profile';
 import About from './pages/About';
 import Header from './components/Header';
+import SignIn from './pages/Signin.jsx';
+import PrivateRoute from './components/PrivateRoute.jsx';
 export default function App() {
   return (
     <BrowserRouter>
@@ -12,9 +14,11 @@ export default function App() {
     <Routes>
       <Route path='/' element={<Home/>}></Route>
       <Route path='/sign-in' element={<SignIn />}></Route>
-      <Route path='/sign-up' element={<SignUp />}></Route>
+     <Route path='sign-up' element={<SignUp/>}/>
       <Route path='/about' element={<About />}></Route>
+      <Route element={<PrivateRoute />}>
       <Route path='/profile' element={<Profile />}></Route>
+      </Route>
       
     </Routes>
     </BrowserRouter>
