aedebca0db52e5c60edf34f7c017a4e677f2354e
[ArhanAli1] <[aliarhan646@gmail.com]>
2023-12-11 11:30:11 +0530

    create search page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './pages/Home';
-
 import SignUp from './pages/SignUp.jsx'
 import Profile from './pages/Profile';
 import About from './pages/About';
@@ -10,6 +9,7 @@ import PrivateRoute from './components/PrivateRoute.jsx';
 import CreateListing from './pages/CreateListing.jsx';
 import UpdateListing from './pages/UpdateListing.jsx';
 import Listing from './pages/Listing.jsx';
+import Search from './pages/Search.jsx';
 export default function App() {
   return (
     <BrowserRouter>
@@ -19,6 +19,7 @@ export default function App() {
       <Route path='/sign-in' element={<SignIn />}></Route>
      <Route path='sign-up' element={<SignUp/>}/>
       <Route path='/about' element={<About />}></Route>
+      <Route path='/search' element={<Search />}></Route>
       <Route path='/listing/:listingId' element={<Listing />}></Route>
       <Route element={<PrivateRoute />}>
       <Route path='/profile' element={<Profile />}></Route>
