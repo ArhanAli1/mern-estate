1b74d172e60ab5198773000b35dc77e135aed0e2
[ArhanAli1] <[aliarhan646@gmail.com]>
2023-12-13 11:45:44 +0530

    remove spaces

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,15 +16,15 @@ export default function App() {
     <Header/>
     <Routes>
       <Route path='/' element={<Home/>}></Route>
-      <Route path='/sign-in' element={<SignIn />}></Route>
+      <Route path='/sign-in' element={<SignIn/>}></Route>
      <Route path='sign-up' element={<SignUp/>}/>
-      <Route path='/about' element={<About />}></Route>
-      <Route path='/search' element={<Search />}></Route>
-      <Route path='/listing/:listingId' element={<Listing />}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/search' element={<Search/>}></Route>
+      <Route path='/listing/:listingId' element={<Listing/>}></Route>
       <Route element={<PrivateRoute />}>
-      <Route path='/profile' element={<Profile />}></Route>
-      <Route path='/create-listing' element={<CreateListing />}></Route>
-      <Route path='/update-listing/:listingId' element={<UpdateListing />}></Route>
+      <Route path='/profile' element={<Profile/>}></Route>
+      <Route path='/create-listing' element={<CreateListing/>}></Route>
+      <Route path='/update-listing/:listingId' element={<UpdateListing/>}></Route>
       </Route>
       
     </Routes>
